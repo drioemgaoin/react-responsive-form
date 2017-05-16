603fa668da3d16d4266916b6f2d32df83027dbc0
Diegoni <romain_diegoni@hotmail.com>
2017-05-16 20:35:00 +0100

    Restore example

--- lib/react-responsive-form.jsx
+++ lib/react-responsive-form.jsx
@@ -76,6 +76,294 @@ export default class ResponsiveForm extends React.Component {
           <pre>{JSON.stringify(this.state.values, (key, value) => value ? value : null, 2)}</pre>
         </div>
         <hr />
+
+        <h2>Default Form validation on change</h2>
+        <Form mode={FormMode.Edit} onFormSubmit={this.onSubmit.bind(this)} validationMode={ValidationMode.OnChange}>
+            <Input ref={(el) => this.firstname = el}
+                name='firstname'
+                label='First Name'
+                placeholder='First Name'
+                isRequired={true} />
+            <InputNumber ref={(el) => this.quantity = el}
+                name='quantity'
+                label='Quantity'
+                placeholder='Quantity'
+                isRequired={true} />
+            <InputEmail ref={(el) => this.email = el}
+                name='email'
+                label='Email'
+                placeholder='example@domain.com'
+                isRequired={true} />
+            <Select ref={(el) => this.gender = el}
+                name='gender'
+                label='Gender'
+                placeholder='Gender'
+                isRequired={true}
+                selectOptionLabel='Select Gender'
+                options={[{id: 1, label: 'Male'}, {id: 2, label: 'Female'}]} />
+            <Textarea ref={(el) => this.note = el}
+                name='note'
+                label='Note'
+                placeholder='Note' />
+            <ListGroup name='service'
+                label='Services'
+                isRequired={true}>
+              <Accordion>
+                <Panel header='Category 1' eventKey='1'>
+                    <ListItem value={0}>
+                        Service 1
+                    </ListItem>
+                    <ListItem value={1}>
+                        Service 2
+                    </ListItem>
+                </Panel>
+                <Panel header='Category 2' eventKey='2'>
+                    <ListItem value={2}>
+                        Service 3
+                    </ListItem>
+                    <ListItem value={3}>
+                        Service 4
+                    </ListItem>
+                </Panel>
+              </Accordion>
+            </ListGroup>
+        </Form>
+        <div id='default-form'>
+          <pre>{JSON.stringify(this.state.values, (key, value) => value ? value : null, 2)}</pre>
+        </div>
+        <hr />
+
+        <h2>Default Form validation on blur</h2>
+        <Form mode={FormMode.Edit} onFormSubmit={this.onSubmit.bind(this)} validationMode={ValidationMode.OnBlur}>
+            <Input ref={(el) => this.firstname = el}
+                name='firstname'
+                label='First Name'
+                placeholder='First Name'
+                isRequired={true} />
+            <InputNumber ref={(el) => this.quantity = el}
+                name='quantity'
+                label='Quantity'
+                placeholder='Quantity'
+                isRequired={true} />
+            <InputEmail ref={(el) => this.email = el}
+                name='email'
+                label='Email'
+                placeholder='example@domain.com'
+                isRequired={true} />
+            <Select ref={(el) => this.gender = el}
+                name='gender'
+                label='Gender'
+                placeholder='Gender'
+                isRequired={true}
+                selectOptionLabel='Select Gender'
+                options={[{id: 1, label: 'Male'}, {id: 2, label: 'Female'}]} />
+            <Textarea ref={(el) => this.note = el}
+                name='note'
+                label='Note'
+                placeholder='Note' />
+            <ListGroup name='service'
+                label='Services'
+                isRequired={true}>
+              <Accordion>
+                <Panel header='Category 1' eventKey='1'>
+                    <ListItem value={0}>
+                        Service 1
+                    </ListItem>
+                    <ListItem value={1}>
+                        Service 2
+                    </ListItem>
+                </Panel>
+                <Panel header='Category 2' eventKey='2'>
+                    <ListItem value={2}>
+                        Service 3
+                    </ListItem>
+                    <ListItem value={3}>
+                        Service 4
+                    </ListItem>
+                </Panel>
+              </Accordion>
+            </ListGroup>
+        </Form>
+        <div id='default-form'>
+          <pre>{JSON.stringify(this.state.values, (key, value) => value ? value : null, 2)}</pre>
+        </div>
+        <hr />
+
+        <h2>Default Form ReadOnly</h2>
+        <Form mode={FormMode.View}>
+            <Input ref={(el) => this.firstname = el}
+                name='firstname'
+                label='First Name'
+                placeholder='First Name'
+                isRequired={true}
+                value='Romain Diegoni' />
+            <InputNumber ref={(el) => this.quantity = el}
+                name='quantity'
+                label='Quantity'
+                placeholder='Quantity'
+                isRequired={true}
+                value={1000} />
+            <InputEmail ref={(el) => this.email = el}
+                name='email'
+                label='Email'
+                placeholder='example@domain.com'
+                isRequired={true}
+                value='custom@domain.com' />
+            <Select ref={(el) => this.gender = el}
+                name='gender'
+                label='Gender'
+                placeholder='Gender'
+                isRequired={true}
+                selectOptionLabel='Select Gender'
+                options={[{id: 1, label: 'Male'}, {id: 2, label: 'Female'}]}
+                value={2} />
+            <Textarea ref={(el) => this.note = el}
+                name='note'
+                label='Note'
+                placeholder='Note'
+                isRequired={true}
+                value='It is a note\nA multi-line one' />
+            <ListGroup name='service'
+                label='Services'
+                isRequired={true}
+                value={2}>
+              <Accordion>
+                <Panel header='Category 1' eventKey='1'>
+                    <ListItem value={0}>
+                        Service 1
+                    </ListItem>
+                    <ListItem value={1}>
+                        Service 2
+                    </ListItem>
+                </Panel>
+                <Panel header='Category 2' eventKey='2'>
+                    <ListItem value={2}>
+                        Service 3
+                    </ListItem>
+                    <ListItem value={3}>
+                        Service 4
+                    </ListItem>
+                </Panel>
+              </Accordion>
+            </ListGroup>
+        </Form>
+        <hr />
+
+        <h2>Default Form Edit</h2>
+        <Form mode={FormMode.Edit}>
+            <Input ref={(el) => this.firstname = el}
+                name='firstname'
+                label='First Name'
+                placeholder='First Name'
+                isRequired={true}
+                value='Romain Diegoni' />
+            <InputNumber ref={(el) => this.quantity = el}
+                name='quantity'
+                label='Quantity'
+                placeholder='Quantity'
+                isRequired={true}
+                maxDecimalPlaces={2}
+                value={1000} />
+            <InputEmail ref={(el) => this.email = el}
+                name='email'
+                label='Email'
+                placeholder='example@domain.com'
+                isRequired={true}
+                value='custom@domain.com' />
+            <Select ref={(el) => this.gender = el}
+                name='gender'
+                label='Gender'
+                placeholder='Gender'
+                isRequired={true}
+                selectOptionLabel='Select Gender'
+                options={[{id: 1, label: 'Male'}, {id: 2, label: 'Female'}]}
+                value={2} />
+            <Textarea ref={(el) => this.note = el}
+                name='note'
+                label='Note'
+                placeholder='Note'
+                isRequired={true}
+                value='It is a note\nA multi-line one' />
+            <ListGroup name='service'
+                label='Services'
+                isRequired={true}
+                value={2}>
+              <Accordion>
+                <Panel header='Category 1' eventKey='1'>
+                    <ListItem value={0}>
+                        Service 1
+                    </ListItem>
+                    <ListItem value={1}>
+                        Service 2
+                    </ListItem>
+                </Panel>
+                <Panel header='Category 2' eventKey='2'>
+                    <ListItem value={2}>
+                        Service 3
+                    </ListItem>
+                    <ListItem value={3}>
+                        Service 4
+                    </ListItem>
+                </Panel>
+              </Accordion>
+            </ListGroup>
+        </Form>
+        <hr />
+
+        <h2>Custom Form validation on submit</h2>
+        <Form className='react-responsive-form' mode={FormMode.Edit} onFormSubmit={this.onSubmit.bind(this)}>
+            <Input ref={(el) => this.firstname = el}
+                name='firstname'
+                label='First Name'
+                placeholder='First Name'
+                isRequired={false} />
+            <InputNumber ref={(el) => this.quantity = el}
+                name='quantity'
+                label='Quantity'
+                placeholder='Quantity'
+                isRequired={true} />
+            <InputEmail ref={(el) => this.email = el}
+                name='email'
+                label='Email'
+                placeholder='example@domain.com'
+                isRequired={true} />
+            <Select ref={(el) => this.gender = el}
+                name='gender'
+                label='Gender'
+                placeholder='Gender'
+                isRequired={true}
+                selectOptionLabel='Select Gender'
+                options={[{id: 1, label: 'Male'}, {id: 2, label: 'Female'}]} />
+            <Textarea ref={(el) => this.note = el}
+                name='note'
+                label='Note'
+                placeholder='Note' />
+            <ListGroup name='service'
+                label='Services'
+                isRequired={true}>
+              <Accordion>
+                <Panel header='Category 1' eventKey='1'>
+                    <ListItem value={0}>
+                        Service 1
+                    </ListItem>
+                    <ListItem value={1}>
+                        Service 2
+                    </ListItem>
+                </Panel>
+                <Panel header='Category 2' eventKey='2'>
+                    <ListItem value={2}>
+                        Service 3
+                    </ListItem>
+                    <ListItem value={3}>
+                        Service 4
+                    </ListItem>
+                </Panel>
+              </Accordion>
+            </ListGroup>
+        </Form>
+        <div id='default-form'>
+          <pre>{JSON.stringify(this.state.values, (key, value) => value ? value : null, 2)}</pre>
+        </div>
       </div>
     );
   }
