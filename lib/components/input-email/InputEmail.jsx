7dabfba02ec748e09e18904435f5092d7bb5001a
Diegoni <romain_diegoni@hotmail.com>
2017-05-16 20:44:07 +0100

    Make inputemail component as controlled component

--- lib/components/input-email/InputEmail.jsx
+++ lib/components/input-email/InputEmail.jsx
@@ -1,12 +1,31 @@
+import React from 'react';
+import classnames from 'classnames';
+
 import validate from './validations';
 import Input from '../input/Input';
 
-export default class InputEmail extends Input {
+import FieldComponent from '../FieldComponent';
+
+export default class InputEmail extends FieldComponent {
   static defaultProps = {
       validate: validate
   };
 
-  constructor(props) {
-    super(props);
+  getValue() {
+    return this.refs[this.name].getValue();
+  }
+
+  setValidationMessages(messages: string[]) {
+    this.refs[this.name].setValidationMessages(messages);
+  }
+
+  render() {
+      const className = classnames('InputEmail', this.props.className);
+      return (
+        <Input className={className}
+               ref={this.name}
+               {...this.props}
+               validate={(value) => validate(value, this.props.maxDecimalPlaces, this.props.min)} />
+      );
   }
 }
